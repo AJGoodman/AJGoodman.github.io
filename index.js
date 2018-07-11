a2baccd9df0edfb65c78f1af1e72ff4ac6a07941
unknown <agoodman@c20g.com>
2018-07-11 16:30:05 -0400

    updated locations

--- index.js
+++ index.js
@@ -14,16 +14,16 @@ function initMap() {
 
 
 	var locations = [
-		new Location('Arlington', {lat:38.88 , lng:-77.11}, 'home'),
-		new Location('San Francisco', {lat:37.79 , lng:-122.42}, 'not home'),
-		new Location('Chicago', {lat:41.95, lng:-87.66}, 'Chi-town'),
-		new Location('Miami', {lat:25.72, lng:-80.28}, 'go canes'),
-		new Location('Paris', {lat:48.85, lng:2.35}, 'bonjour'),
-		new Location('London', {lat:51.51, lng:-0.09}, 'fish and chips'),
-		new Location('Sydney', {lat:-33.87, lng:151.21}, 'down under'),
-		new Location('Queenstown', {lat:-45.03, lng:168.66}, 'kiwis'),
-		new Location('Playa Del Carmen', {lat:20.63, lng:-87.06}, 'hola'),
-		new Location('Montego Bay', {lat:18.46, lng:-78.01}, 'hola'),
+		new Location('Arlington', {lat:38.88 , lng:-77.11}, locationInfo.arlington),
+		new Location('San Francisco', {lat:37.79 , lng:-122.42}, locationInfo.sanFrancisco),
+		new Location('Chicago', {lat:41.95, lng:-87.66}, locationInfo.chicago),
+		new Location('Miami', {lat:25.72, lng:-80.28}, locationInfo.miami),
+		new Location('Paris', {lat:48.85, lng:2.35}, locationInfo.paris),
+		new Location('London', {lat:51.51, lng:-0.09}, locationInfo.london),
+		new Location('Sydney', {lat:-33.87, lng:151.21}, locationInfo.sydney),
+		new Location('Queenstown', {lat:-45.03, lng:168.66}, locationInfo.queenstown),
+		new Location('Playa Del Carmen', {lat:20.63, lng:-87.06}, locationInfo.playaDelCarmen),
+		new Location('Montego Bay', {lat:18.46, lng:-78.01}, locationInfo.montegoBay),
 		//new Location('Costa Rica', {lat:, lng:}, ''),
 		new Location('Milan', {lat:45.46, lng:9.19}, locationInfo.milan),
 		new Location('Lake Como', {lat:45.99, lng:9.26}, locationInfo.lakeComo),
@@ -33,6 +33,15 @@ function initMap() {
 		new Location('Reynisfjara', {lat:63.41, lng:-19.09}, locationInfo.reynisfjara),
 		new Location('Skogafoss', {lat:63.53, lng:-19.51}, locationInfo.skogafoss),
 		new Location('Svinafellsjokull', {lat:63.98, lng:-16.96}, locationInfo.svinafellsjokull),
+		new Location('Alaska', {lat:61.22, lng:-149.90}, locationInfo.alaska),
+		new Location('Hawaii', {lat:21.28, lng:-157.83}, locationInfo.hawaii),
+		new Location('Lake McAlester', {lat:35.03, lng:-95.83}, locationInfo.lakeMcAlester),
+		new Location('Big Sky', {lat:45.28, lng:-111.40}, locationInfo.bigSky),
+		new Location('Wisp', {lat:39.56, lng:-79.36}, locationInfo.wisp),
+		new Location('New Orleans', {lat:29.95, lng:-90.07}, locationInfo.newOrleans),
+		new Location('Las Vegas', {lat:36.11, lng:-115.17}, locationInfo.lasVegas),
+		new Location('Dublin', {lat:53.35, lng:-6.26}, locationInfo.dublin),
+
 
 	]
 
--- locations.js
+++ locations.js
@@ -1,22 +1,53 @@
 var locationInfo = {
 
-	venice : "<div><img src='./BridgeOfSighs.jpg' class='infoWindowImage'></div><div><p>Venice: In front of the famous Bridge of Sighs in Venice.  The bridge connected the prison with the Doge\'s palace, where all cases were heard.  It was named the bridge of sighs because prisoners walking across would glimpse out to the lagoon, catch their last look at the outdoors, and let out a sigh. May 2018</p></div>",
+	arlington : "<div></div><div><p>home</p></div>",
+
+	sanFrancisco : "<div></div><div><p>Sarah</p></div>",
+
+	chicago : "<div></div><div><p>chi-town</p></div>",
+
+	miami : "<div></div><div><p>go canes</p></div>",
+
+	paris : "<div></div><div><p>bonjour</p></div>",
+
+	london : "<div></div><div><p>fish and chips</p></div>",
+
+	sydney : "<div></div><div><p>down under</p></div>",
+
+	queenstown: "<div></div><div><p>kiwis</p></div>",
+
+	playaDelCarmen : "<div></div><div><p>hola</p></div>",
+
+	montegoBay : "<div></div><div><p> August 2017</p></div>",
+
+	milan : "<div><img src='./MilanDuomo.jpg' class='infoWindowImage'></div><div><p>In front of the Duomo.  The maginificent cathedral is the largest church in Italy and is covered in hundreds of unique sculptures as well as artful stained glass windows.  May 2018</p></div>",
 
 	lakeComo : "<div><img src='./LakeComo.jpg' class='infoWindowImage'></div><div><p>Overlooking beautiful Lake Como.  The picture was taken from the Villa Serbelloni which commands stunning 360° of the lake and is currently owned and operated by the Rockefeller foundation as a retreat for writers, artists, musicians, and thinkers.  May 2018</p></div>",
 
-	milan: "<div><img src='./MilanDuomo.jpg' class='infoWindowImage'></div><div><p>In front of the Duomo.  The maginificent cathedral is the largest church in Italy and is covered in hundreds of unique sculptures as well as artful stained glass windows.  May 2018</p></div>",
+	venice : "<div><img src='./BridgeOfSighs.jpg' class='infoWindowImage'></div><div><p>Venice: In front of the famous Bridge of Sighs in Venice.  The bridge connected the prison with the Doge\'s palace, where all cases were heard.  It was named the bridge of sighs because prisoners walking across would glimpse out to the lagoon, catch their last look at the outdoors, and let out a sigh. May 2018</p></div>",
 
-	reynisfjara: "<div><img src='./Reynisfjara.jpg' class'infoWindowImage'></div><div><p>Reynisfjara beach is famous for its black sand and basalt columns.  On this particular day it was quite wet and windy.  August 2017</p></div>",
+	niagaraFalls : "<div></div><div><p> August 2017</p></div>",
+
+	reynisfjara : "<div><img src='./Reynisfjara.jpg' class'infoWindowImage'></div><div><p>Reynisfjara beach is famous for its black sand and basalt columns.  On this particular day it was quite wet and windy.  August 2017</p></div>",
 	
-	skogafoss: "<div><img src='./Skogafoss.jpg' class='infoWindowImage'></div><div><p></p></div>",
+	skogafoss : "<div><img src='./Skogafoss.jpg' class='infoWindowImage'></div><div><p></p></div>",
 
-	svinafellsjokull: "<div><img src='./Iceland.jpg' class='infoWindowImage'></div><div><p>In front of Svinafellsjokull which in English means Pig Mountain Glacier.  Multiple movies and TV shows have been filmed here including Interstellar, Batman Begins, and Game of Thrones.  August 2017</p></div>",
+	svinafellsjokull : "<div><img src='./Iceland.jpg' class='infoWindowImage'></div><div><p>In front of Svinafellsjokull which in English means Pig Mountain Glacier.  Multiple movies and TV shows have been filmed here including Interstellar, Batman Begins, and Game of Thrones.  August 2017</p></div>",
 
-	queenstown: "<div></div><div><p>April 2012</p></div>",
+	alaska : "<div></div><div><p>there was a great big moose</p></div>",
 
-	alaska: "<div></div><div><p>August 2016</p></div>",
+	hawaii : "<div></div><div><p>aloha</p></div>",
 
-	montegoBay: "<div></div><div><p> August 2017</p></div>",
+	lakeMcAlester : "<div></div><div><p>I learned to swim in Lake McAlester when my cousins Bobby and Andrew tossed me in and told me I had two options: sink or swim.  I'm sure they would have rescued me if it had been necessary...</p></div>",
 
-	paris: ""
+	bigSky : "<div></div><div><p>January 2018</p></div>",
+
+	wisp : "<div></div><div><p>January 2018</p></div>",
+
+	newOrleans : "<div></div><div><p>January 2018</p></div>",
+
+	lasVegas : "<div></div><div><p>January 2018</p></div>",
+
+	dublin : "<div></div><div><p>January 2018</p></div>",
+
 };
