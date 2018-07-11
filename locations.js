56eabd5d33ef61d7ae0163ba989f656e3aee4ec3
unknown <agoodman@c20g.com>
2018-07-11 16:49:39 -0400

    moved all images to ./Photos/

--- locations.js
+++ locations.js
@@ -20,19 +20,19 @@ var locationInfo = {
 
 	montegoBay : "<div></div><div><p> August 2017</p></div>",
 
-	milan : "<div><img src='./MilanDuomo.jpg' class='infoWindowImage'></div><div><p>In front of the Duomo.  The maginificent cathedral is the largest church in Italy and is covered in hundreds of unique sculptures as well as artful stained glass windows.  May 2018</p></div>",
+	milan : "<div><img src='./Photos/MilanDuomo.jpg' class='infoWindowImage'></div><div><p>In front of the Duomo.  This maginificent cathedral is the largest church in Italy and is covered in hundreds of unique sculptures as well as artful stained glass windows.  May 2018</p></div>",
 
-	lakeComo : "<div><img src='./LakeComo.jpg' class='infoWindowImage'></div><div><p>Overlooking beautiful Lake Como.  The picture was taken from the Villa Serbelloni which commands stunning 360° of the lake and is currently owned and operated by the Rockefeller foundation as a retreat for writers, artists, musicians, and thinkers.  May 2018</p></div>",
+	lakeComo : "<div><img src='./Photos/LakeComo.jpg' class='infoWindowImage'></div><div><p>Overlooking beautiful Lake Como.  The picture was taken from the Villa Serbelloni which commands stunning 360° of the lake and is currently owned and operated by the Rockefeller foundation as a retreat for writers, artists, musicians, and thinkers.  May 2018</p></div>",
 
-	venice : "<div><img src='./BridgeOfSighs.jpg' class='infoWindowImage'></div><div><p>Venice: In front of the famous Bridge of Sighs in Venice.  The bridge connected the prison with the Doge\'s palace, where all cases were heard.  It was named the bridge of sighs because prisoners walking across would glimpse out to the lagoon, catch their last look at the outdoors, and let out a sigh. May 2018</p></div>",
+	venice : "<div><img src='./Photos/BridgeOfSighs.jpg' class='infoWindowImage'></div><div><p>Venice: In front of the famous Bridge of Sighs in Venice.  The bridge connected the prison with the Doge\'s palace, where all cases were heard.  It was named the bridge of sighs because prisoners walking across would glimpse out to the lagoon, catch their last look at the outdoors, and let out a sigh. May 2018</p></div>",
 
 	niagaraFalls : "<div></div><div><p> August 2017</p></div>",
 
-	reynisfjara : "<div><img src='./Reynisfjara.jpg' class'infoWindowImage'></div><div><p>Reynisfjara beach is famous for its black sand and basalt columns.  On this particular day it was quite wet and windy.  August 2017</p></div>",
+	reynisfjara : "<div><img src='./Photos/Reynisfjara.jpg' class='infoWindowImage'></div><div><p>Reynisfjara beach is famous for its black sand and basalt columns.  On this particular day it was quite wet and windy.  August 2017</p></div>",
 	
-	skogafoss : "<div><img src='./Skogafoss.jpg' class='infoWindowImage'></div><div><p></p></div>",
+	skogafoss : "<div><img src='./Photos/Skogafoss.jpg' class='infoWindowImage'></div><div><p></p></div>",
 
-	svinafellsjokull : "<div><img src='./Iceland.jpg' class='infoWindowImage'></div><div><p>In front of Svinafellsjokull which in English means Pig Mountain Glacier.  Multiple movies and TV shows have been filmed here including Interstellar, Batman Begins, and Game of Thrones.  August 2017</p></div>",
+	svinafellsjokull : "<div><img src='./Photos/Iceland.jpg' class='infoWindowImage'></div><div><p>In front of Svinafellsjokull which translated to English means Pig Mountain Glacier.  Multiple movies and TV shows have been filmed here including Interstellar, Batman Begins, and Game of Thrones.  August 2017</p></div>",
 
 	alaska : "<div></div><div><p>there was a great big moose</p></div>",
 
@@ -40,7 +40,7 @@ var locationInfo = {
 
 	lakeMcAlester : "<div></div><div><p>I learned to swim in Lake McAlester when my cousins Bobby and Andrew tossed me in and told me I had two options: sink or swim.  I'm sure they would have rescued me if it had been necessary...</p></div>",
 
-	bigSky : "<div></div><div><p>January 2018</p></div>",
+	bigSky : "<div><img src='./Photos/BigSky.jpg' class='infoWindowImage'></div><div><p>Riding the chairlift on a snowy day with my buddy Dane.  January 2018</p></div>",
 
 	wisp : "<div></div><div><p>January 2018</p></div>",
 
